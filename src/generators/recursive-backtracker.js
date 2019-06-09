4d7d583c5de1861c77e25850ea540b6b534063ac
Bill Reed <billnreed@gmail.com>
2019-06-09 16:05:35 -0400

    pull out algorithm to generator class

---
+++ src/generators/recursive-backtracker.js
@@ -0,0 +1,69 @@
+export class RecursiveBacktracker {
+  constructor(scene, grid) {
+    this.scene = scene;
+    this.grid = grid;
+  }
+
+  generate() {
+    const TIME_STEP = 50;
+    let destroyedWallCount = 0;
+
+    let currentRow = 0;
+    let currentCol = 0;
+    const cellStack = [];
+    this.grid.get(currentRow, currentCol).visited = true;
+
+    this.grid.get(currentRow, currentCol).removeTopWall();
+    this.grid.get(this.grid.height - 1, this.grid.width - 1).removeBottomWall();
+
+    while (this.grid.some(cell => !cell.visited)) {
+      const cell = this.grid.get(currentRow, currentCol);
+      const neighbors = this.grid.getNeighbors(currentRow, currentCol);
+      const unvisitedNeighbors = neighbors.filter(neighbor => !neighbor.visited);
+
+      if (unvisitedNeighbors.length) {
+        const neighbor = Phaser.Math.RND.pick(unvisitedNeighbors);
+        cellStack.push(cell);
+
+        if (cell.row == neighbor.row) {
+          if (neighbor.col < cell.col) { // left
+            this.scene.time.delayedCall(TIME_STEP * destroyedWallCount, () => {
+              neighbor.removeRightWall();
+              cell.removeLeftWall();
+            });
+            destroyedWallCount += 1;
+          } else { // right
+            this.scene.time.delayedCall(TIME_STEP * destroyedWallCount, () => {
+              neighbor.removeLeftWall();
+              cell.removeRightWall();
+            });
+            destroyedWallCount += 1;
+          }
+        } else {
+          if (neighbor.row < cell.row) { // above
+            this.scene.time.delayedCall(TIME_STEP * destroyedWallCount, () => {
+              neighbor.removeBottomWall();
+              cell.removeTopWall();
+            });
+            destroyedWallCount += 1;
+          } else { // below
+            this.scene.time.delayedCall(TIME_STEP * destroyedWallCount, () => {
+              neighbor.removeTopWall();
+              cell.removeBottomWall();
+            });
+            destroyedWallCount += 1;
+          }
+        }
+
+        currentRow = neighbor.row;
+        currentCol = neighbor.col;
+
+        neighbor.visited = true;
+      } else if (cellStack.length) {
+        const poppedCell = cellStack.pop();
+        currentRow = poppedCell.row;
+        currentCol = poppedCell.col;
+      }
+    }
+  }
+}
--- src/scenes/maze-scene.js
+++ src/scenes/maze-scene.js
@@ -1,79 +1,18 @@
 import 'phaser'
 import { Grid } from '../grid';
+import { RecursiveBacktracker } from '../generators/recursive-backtracker';
 
 export class MazeScene extends Phaser.Scene {
   create() {
     this.cameras.main.setBackgroundColor(0xcacaca);
 
-    this.ROWS = 10;
-    this.COLS = 10;
-    const size = 40;
+    const rows = 10;
+    const cols = 10;
+    const cellSize = 40;
 
-    this.grid = new Grid(this, this.ROWS, this.COLS, size);
+    const grid = new Grid(this, rows, cols, cellSize);
 
-    this.generateMaze();
-  }
-
-  generateMaze() {
-    const TIME_STEP = 50;
-    let destroyedWallCount = 0;
-
-    let currentRow = 0;
-    let currentCol = 0;
-    const cellStack = [];
-    this.grid.get(currentRow, currentCol).visited = true;
-
-    this.grid.get(currentRow, currentCol).removeTopWall();
-    this.grid.get(this.ROWS - 1, this.COLS - 1).removeBottomWall();
-
-    while (this.grid.some(cell => !cell.visited)) {
-      const cell = this.grid.get(currentRow, currentCol);
-      const neighbors = this.grid.getNeighbors(currentRow, currentCol);
-      const unvisitedNeighbors = neighbors.filter(neighbor => !neighbor.visited);
-
-      if (unvisitedNeighbors.length) {
-        const neighbor = Phaser.Math.RND.pick(unvisitedNeighbors);
-        cellStack.push(cell);
-
-        if (cell.row == neighbor.row) {
-          if (neighbor.col < cell.col) { // left
-            this.time.delayedCall(TIME_STEP * destroyedWallCount, () => {
-              neighbor.removeRightWall();
-              cell.removeLeftWall();
-            });
-            destroyedWallCount += 1;
-          } else { // right
-            this.time.delayedCall(TIME_STEP * destroyedWallCount, () => {
-              neighbor.removeLeftWall();
-              cell.removeRightWall();
-            });
-            destroyedWallCount += 1;
-          }
-        } else {
-          if (neighbor.row < cell.row) { // above
-            this.time.delayedCall(TIME_STEP * destroyedWallCount, () => {
-              neighbor.removeBottomWall();
-              cell.removeTopWall();
-            });
-            destroyedWallCount += 1;
-          } else { // below
-            this.time.delayedCall(TIME_STEP * destroyedWallCount, () => {
-              neighbor.removeTopWall();
-              cell.removeBottomWall();
-            });
-            destroyedWallCount += 1;
-          }
-        }
-
-        currentRow = neighbor.row;
-        currentCol = neighbor.col;
-
-        neighbor.visited = true;
-      } else if (cellStack.length) {
-        const poppedCell = cellStack.pop();
-        currentRow = poppedCell.row;
-        currentCol = poppedCell.col;
-      }
-    }
+    const generator = new RecursiveBacktracker(this, grid);
+    generator.generate();
   }
 }
